da604d7f91eca6fd22cc190f651c99807a7d7606
nerver111 <1617949175@qq.com>
2025-05-22 13:52:58 +0800

    123

--- srv/salesplan-service.js
+++ srv/salesplan-service.js
@@ -1,17 +1,15 @@
 const cds = require('@sap/cds');
 
 module.exports = cds.service.impl(async function() {
-  const { SalesPlan } = this.entities;
+  const { SalesPlans } = this.entities;
 
-  this.on('READ', SalesPlan, async (req, next) => {
+  this.on('READ', SalesPlans, async (req, next) => {
     const user = req.user;
-    // 根据角色集合过滤数据
     if (user.is('HyperManager')) {
       req.query.where({ nkaType: 'Hyper' });
     } else if (user.is('NKAManager')) {
       req.query.where({ nkaType: 'NKA' });
     }
-    // 其它角色可继续扩展
     return next();
   });
-}); 
+});
